90d24d117154c93793cf565d4f8c16adb9dac03b
GulTion <gkv8192@gmail.com>
2021-08-03 09:51:36 +0530

    worker

--- worker.py
+++ worker.py
@@ -1,21 +1,54 @@
 import json
 import os
 
-f = open("folders/[APPLIED] GATE 52.83GBo1hjHksjxMKbbIs3EOaR5f80qPDsw9ecgS.json")
+f = open("/content/DriveVideoDownloader/--Mastering Data Structures & Algorithms using C and C++o1B3XH4PTmpUerDDkLnxqxgoKtfo6XuY9T.json")
 
 dump = json.loads(f.read())
-print(dump['name'])
+
+import requests
+from urllib.parse import unquote
+import sys
+
+arg = sys.argv
+
+def downloader(id, path, q):
+    url = f"https://docs.google.com/get_video_info?docid={id}"
+    s= requests.Session()
+    r = s.get(url)
+    data = r.content.decode()
+    urlDownload = parseURL(data)[str(q)]
+    r = s.get(urlDownload)
+    with open(path,'wb') as f:
+        f.write(r.content)
+        print(path)
+
+def parseURL(urlC):
+    qual = {}
+    urlC = urlC.split('&')
+    fmt_map = urlC[17]
+    qstring = fmt_map[15:].split("%2C")
+    for i in qstring:
+        sma = unquote(i).split("|")
+        qual[sma[0]] = sma[1]
+    
+    return qual
+
+
+# urlDownload = parseUrl(data)
+
+
+
 def recursion(obj, p):
     if obj.get("type")=='folder':
-        os.popen(f'cd "{p}" & mkdir "{obj["""name"""]}"')
+        !cd {'"'+p+'"'} && mkdir {'"'+obj.get("name")+'"'}
         for folder in obj['items']:
             recursion(folder, p+"/"+obj['name'])
     else:
         if obj.get("mimeType")=="video/mp4":
             # print(p+"/"+obj['name'])
             g = p+"/"+obj['name']
-            os.popen(f'python3 ./downloader.py {obj.get(id)} {g}')
+            os.popen(f'python /content/DriveVideoDownloader/downloader.py {obj.get("id")} "{g}" 22')
 
 
-recursion(dump, "df")
-#0.0.4
+recursion(dump, "/content/drive/MyDrive/unflag/18")
+#0.0.2
